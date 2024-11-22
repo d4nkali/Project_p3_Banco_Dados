14b3a2c78d0a482991b72a4e8f0984b0b5a442e8
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-11-22 16:56:41 -0300

    Adicionando pasta api

---
+++ api/src/main/java/br/com/criaturaspedia/api/CriaturasPediaApplication.java
@@ -0,0 +1,13 @@
+package br.com.criaturaspedia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriaturasPediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriaturasPediaApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/criaturaspedia/api/CriaturasPediaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.criaturaspedia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriaturasPediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
