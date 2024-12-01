eb35cf437e3f1bc19e0823947e664bbb37c4785b
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-11-30 22:01:37 -0300

    Terminando crud Habilidade

--- api/src/main/java/br/com/criaturaspedia/api/controller/HabilidadeController.java
+++ api/src/main/java/br/com/criaturaspedia/api/controller/HabilidadeController.java
@@ -1,8 +1,15 @@
 package br.com.criaturaspedia.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,6 +30,36 @@ public class HabilidadeController {
 	public void criarHabilidade(@RequestBody Habilidade habilidade) {
 
 		habilidadeService.criarHabilidade(habilidade);
+
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Habilidade> listarTodasHabilidades() {
+
+		return habilidadeService.listarTodasHabilidades();
+
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<Habilidade> buscarHabilidadePorId(@PathVariable Long id) {
+		return habilidadeService.buscarHabilidadePorId(id);
+	}
+
+	@DeleteMapping("/{id}") // Mapeia para usar o método Delete
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Retorna o código 204(No Content)
+	public void deletarHabilidadePorId(@PathVariable Long id) {
+
+		habilidadeService.deletarHabilidadePorId(id);
+
+	}
+
+	@PutMapping("/{id}") // Mapeia para usar o método Put
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarHabilidadePorId(@PathVariable Long id, @RequestBody Habilidade habilidade) {
+
+		habilidadeService.atualizarHabilidadePorId(id, habilidade);
 
 	}
 
--- api/src/main/java/br/com/criaturaspedia/api/service/HabilidadeService.java
+++ api/src/main/java/br/com/criaturaspedia/api/service/HabilidadeService.java
@@ -1,7 +1,12 @@
 package br.com.criaturaspedia.api.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.criaturaspedia.api.model.Habilidade;
 import br.com.criaturaspedia.api.repository.HabilidadeRepository;
@@ -15,6 +20,43 @@ public class HabilidadeService {
 	public void criarHabilidade(Habilidade habilidade) {
 
 		habilidadeRepository.save(habilidade);
+
+	}
+
+	public List<Habilidade> listarTodasHabilidades() {
+
+		return habilidadeRepository.findAll();
+
+	}
+
+	public Optional<Habilidade> buscarHabilidadePorId(Long id) {
+
+		return habilidadeRepository.findById(id);
+
+	}
+
+	public void deletarHabilidadePorId(Long id) {
+
+		habilidadeRepository.deleteById(id);
+
+	}
+
+	public void atualizarHabilidadePorId(Long id, Habilidade habilidade) {
+
+		Optional<Habilidade> habilidadeDoBanco = buscarHabilidadePorId(id);
+
+		if (habilidadeDoBanco.isEmpty()) {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Habilidade não encontrada no banco de dados");
+
+		}
+
+		Habilidade habilidadeEditada = habilidadeDoBanco.get();
+
+		habilidadeEditada.setNome(habilidade.getNome());
+		habilidadeEditada.setDescricao(habilidade.getDescricao());
+
+		habilidadeRepository.save(habilidadeEditada);
 
 	}
 
