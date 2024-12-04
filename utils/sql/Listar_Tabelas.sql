b787a256db5f9b803d8006a54b6cec49f68332ec
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-12-04 16:52:41 -0300

    Backup do banco de dados

---
+++ utils/sql/Listar_Tabelas.sql
@@ -0,0 +1,11 @@
+/*
+Scripts para listar as tabelas do banco
+*/
+-- Listar Habilidade
+SELECT * FROM habilidade;
+
+-- Listar Habitat
+SELECT * FROM habitat;
+
+-- Listar Criatura
+SELECT * FROM habitat;
